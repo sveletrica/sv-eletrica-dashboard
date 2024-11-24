699f931dd82ed132af3f4bb7af593a5bbae72aef
Diogo <diogo@sveletrica.com>
2024-11-24 13:20:33 -0300

    cards grid cols 2

--- app/vendas-dia/[cdpedido]/page.tsx
+++ app/vendas-dia/[cdpedido]/page.tsx
@@ -120,7 +120,7 @@ export default function SaleDetails() {
                 Voltar
             </Button>
 
-            <div className="grid gap-4 md:grid-cols-4">
+            <div className="grid gap-4 grid-cols-2 sm:grid-cols-4 md:grid-cols-4">
                 <Card>
                     <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                         <CardTitle className="text-sm font-medium">
